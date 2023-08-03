4484d79d1c9624cca0328f5bb57e5bc2cc226bf7
Natalia Savchenko <n.savchenko@gmarkg.com>
2023-08-03 21:59:37 +0300

    feat: implement function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const numbersToWords = {
+        0: 'zero',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen',
+        20: 'twenty',
+        30: 'thirty',
+        40: 'forty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety',
+    }
+
+    if (number <= 20 || (number <= 90 && number % 10 === 0)) {
+        return numbersToWords[number];
+
+    } else if (number > 20 && number <= 99) {
+        return `${numbersToWords[Math.floor(number / 10) + '0']} ${numbersToWords[number % 10]}`;
+
+    } else if (number > 99 && number < 1000) {
+        if (number % 100 === 0) {
+            return `${numbersToWords[Math.floor(number / 100)]} hundred`;
+
+        } else if (number % 100 <= 20 || (number % 100 <= 90 && number % 10 === 0)) {
+            return `${numbersToWords[Math.floor(number / 100)]} hundred ${numbersToWords[number % 100]}`;
+
+        } else {
+            return `${numbersToWords[Math.floor(number / 100)]} hundred ${numbersToWords[Math.floor((number % 100) / 10) + '0']} ${numbersToWords[number % 10]}`;
+        }
+    }
 }
